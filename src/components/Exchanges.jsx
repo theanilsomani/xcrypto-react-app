8ff0a3275dcfb52a8ff9c1c6ba578d1aadc60ced
Anil Somani <129434364+theanilsomani@users.noreply.github.com>
2023-10-29 21:17:53 +0530

    files updated

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -1,22 +1,26 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import {server} from '../index'
-import { Container, HStack, Heading, Image, Text, VStack } from '@chakra-ui/react';
-import Loader from './Loader'
-// import { transform } from 'framer-motion';
-import ErrorComponent from './ErrorComponent';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { server } from "../index";
+import {
+  Container,
+  Heading,
+  HStack,
+  Image,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import Loader from "./Loader";
+import ErrorComponent from "./ErrorComponent";
 
 const Exchanges = () => {
-
   const [exchanges, setExchanges] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
 
   useEffect(() => {
-    const fetchExchanges = async() => {
-
+    const fetchExchanges = async () => {
       try {
-        const {data} = await axios.get(`${server}/exchanges`);
+        const { data } = await axios.get(`${server}/exchanges`);
         setExchanges(data);
         setLoading(false);
       } catch (error) {
@@ -27,37 +31,61 @@ const Exchanges = () => {
     fetchExchanges();
   }, []);
 
-  if (error) 
-    return <ErrorComponent message={'Error while featching exchanges'} />;
-  
+  if (error)
+    return <ErrorComponent message={"Error While Fetching Exchanges"} />;
+
   return (
-    <Container maxW={'container.xl'}>
-      {loading ? <Loader /> : (
+    <Container maxW={"container.xl"}>
+      {loading ? (
+        <Loader />
+      ) : (
         <>
-          <HStack wrap={'wrap'} justifyContent={'space-evenly'}>
+          <HStack wrap={"wrap"} justifyContent={"space-evenly"}>
             {exchanges.map((i) => (
-                <ExchangeCard key={i.id} name={i.name} img={i.image} rank={i.trust_score_rank} url={i.url} />
-              ))}
+              <ExchangeCard
+                key={i.id}
+                name={i.name}
+                img={i.image}
+                rank={i.trust_score_rank}
+                url={i.url}
+              />
+            ))}
           </HStack>
         </>
       )}
     </Container>
-  )
-}
+  );
+};
 
-const ExchangeCard =  ({name, img, rank, url}) => (
-  <a href={url} target='blank'>
-    <VStack w={'52'} shadow={'lg'} p={'8'} borderRadius={'lg'} transition={'all 0.3s'} m={'4'}
-    css={{
-      "&:hover":{
-        transform:"scale(1.1)",
-      }
-    }}>
-      <Image src={img} w={'10'} h={'10'} objectFit={'contain'} alt='Exchange' />
-      <Heading size={'md'} noOfLines={1}>{rank}</Heading>
+const ExchangeCard = ({ name, img, rank, url }) => (
+  <a href={url} target={"blank"}>
+    <VStack
+      w={"52"}
+      shadow={"lg"}
+      p={"8"}
+      borderRadius={"lg"}
+      transition={"all 0.3s"}
+      m={"4"}
+      css={{
+        "&:hover": {
+          transform: "scale(1.1)",
+        },
+      }}
+    >
+      <Image
+        src={img}
+        w={"10"}
+        h={"10"}
+        objectFit={"contain"}
+        alt={"Exchange"}
+      />
+      <Heading size={"md"} noOfLines={1}>
+        {rank}
+      </Heading>
+
       <Text noOfLines={1}>{name}</Text>
     </VStack>
   </a>
-)
+);
 
-export default Exchanges
+export default Exchanges;
